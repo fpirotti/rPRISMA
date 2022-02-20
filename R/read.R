edd5c286054962499c60bf807dc14d7ac614cc65
fpirotti <fpirotti@gmail.com>
2022-02-20 20:17:55 +0100

    fix6

--- R/read.R
+++ R/read.R
@@ -31,12 +31,12 @@ PRISMA2geotiff<-function(input, output= NA, overwrite=F, verbose=F){
     bn<-tools::file_path_sans_ext(bn)
 
     if(ext=="zip"){
-      # if(verbose) message("Unzipping file...")
-      message("Sorry, you have to unzip your file first")
-      return(NULL)
-      # ll <- utils::unzip(input, overwrite = overwrite, exdir = dn, list=TRUE)
-      # utils::unzip(input, overwrite = overwrite, exdir = dn)
-      # input <- file.path(dn,ll$Name)
+      if(verbose) message("Unzipping file...")
+      # message("Sorry, you have to unzip your file first")
+      # return(NULL)
+      ll <- utils::unzip(input, overwrite = overwrite, exdir = dn, list=TRUE)
+      utils::unzip(input, overwrite = overwrite, exdir = dn)
+      input <- file.path(dn,ll$Name)
     }
     bricks<-PRISMA2rast(input, verbose=verbose)
   } else if(is.list(input)){
